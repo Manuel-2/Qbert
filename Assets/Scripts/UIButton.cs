8b59f5cfad4bb356bfb882463951dbb60641cfd0
MaPe-UABCS <mape_23@alu.uabcs.mx>
2023-09-21 22:41:28 -0700

    ahora el fondo es lo que se mueve, no la camara

---
+++ Assets/Scripts/UIButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIButton : MonoBehaviour
+{
+    [SerializeField] RectTransform rectTransform;
+    [SerializeField] Button button;
+
+    private void OnMouseEnter()
+    {
+        rectTransform.localScale = new Vector2(10, 50);
+        Debug.Log("entro");
+    }
+}
